cc42f52242cb0046ac974567487d98cef53698fc
Jineon Baek <jineon@umich.edu>
2023-03-24 19:25:22 -0400

    coprime num and denom

--- src/corollaries/no_parametrization.lean
+++ src/corollaries/no_parametrization.lean
@@ -11,7 +11,50 @@ open ratfunc
 
 variables {k: Type*} [field k]
 
+private lemma aux_is_unit {a : k[X]} (ha : a ≠ 0) : 
+  is_unit (polynomial.C (a.leading_coeff⁻¹)) :=
+begin
+  rw polynomial.is_unit_C, 
+  apply is_unit.inv,
+  rw ←polynomial.leading_coeff_ne_zero at ha, 
+  exact ne.is_unit ha,
+end
+
+private lemma div_mul_eq {a b : k[X]} (h : a ∣ b) : 
+  b / a * a = b :=
+begin
+  rw ←euclidean_domain.mod_eq_zero at h,
+  have t := euclidean_domain.div_add_mod b a,
+  rw [h, add_zero, mul_comm] at t, exact t,
+end
+
+theorem is_coprime.num_denom (x : ratfunc k) : is_coprime x.num x.denom :=
+begin
+  apply ratfunc.induction_on x,
+  intros p q q_nz,
+  rw [ratfunc.num_div, ratfunc.denom_div _ q_nz],
+  have hnz : q / gcd p q ≠ 0 := begin 
+    intro h, apply q_nz,
+    rw ←(div_mul_eq (gcd_dvd_right p q)),
+    rw [h, zero_mul],
+  end,
+  rw is_coprime_mul_unit_left_left 
+    (aux_is_unit hnz) (p / gcd p q) _,
+  rw is_coprime_mul_unit_left_right 
+    (aux_is_unit hnz) (p / gcd p q) _,
+  clear hnz,
+  rw ←gcd_is_unit_iff,
+  have hnz : gcd p q ≠ 0 := gcd_ne_zero_of_right _ _ q_nz,
+  refine is_unit_of_associated_mul _ hnz,
+  apply associated.trans (gcd_mul_left' _ _ _).symm,
+  rw [mul_comm (gcd p q), mul_comm (gcd p q)],
+  rw [div_mul_eq (gcd_dvd_left p q), div_mul_eq (gcd_dvd_right p q)],
+end
+
 def is_const (x : ratfunc k) := ∃ c : k, x = ratfunc.C c
 
 theorem no_parametrization_y2_x3_1 
-  {x y : ratfunc k} (eqn : y^2 = x^3 + 1) : is_const x ∧ is_const y := sorry
+  {x y : ratfunc k} (eqn : y^2 = x^3 + 1) : is_const x ∧ is_const y :=
+begin
+  sorry,
+end
