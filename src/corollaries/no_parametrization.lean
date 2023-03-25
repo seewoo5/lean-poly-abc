e83411e7c9f3716a3fde7b6941ed2a7aa92431df
Jineon Baek <jineon@umich.edu>
2023-03-25 16:02:33 -0400

    factor out elliptic curve eq

--- src/corollaries/no_parametrization.lean
+++ src/corollaries/no_parametrization.lean
@@ -1,3 +1,4 @@
+import tactic.core
 import ring_theory.euclidean_domain
 import ring_theory.polynomial.content
 import field_theory.ratfunc
@@ -56,5 +57,54 @@ def is_const (x : ratfunc k) := ∃ c : k, x = ratfunc.C c
 theorem no_parametrization_y2_x3_1 
   {x y : ratfunc k} (eqn : y^2 = x^3 + 1) : is_const x ∧ is_const y :=
 begin
+  have eq_x := x.num_div_denom.symm,
+  have eq_y := y.num_div_denom.symm,
+  have nz_M := x.denom_ne_zero,
+  have nz_N := y.denom_ne_zero,
+  set m := x.num with eq_m,
+  set M := x.denom with eq_M,
+  set n := y.num with eq_n,
+  set N := y.denom with eq_N,
+  clear_value m M n N,
+  revert m M n N eq_m eq_M eq_n eq_N nz_M nz_N eq_x eq_y,
+  intros m M n N eq_m eq_M eq_n eq_N nz_M nz_N eq_x eq_y,
+  have nz_rM := ratfunc.algebra_map_ne_zero nz_M,
+  have nz_rN := ratfunc.algebra_map_ne_zero nz_N,
+  set rm := algebra_map k[X] (ratfunc k) m with eq_rm,
+  set rM := algebra_map k[X] (ratfunc k) M with eq_rM,
+  set rn := algebra_map k[X] (ratfunc k) n with eq_rn,
+  set rN := algebra_map k[X] (ratfunc k) N with eq_rN,
+  clear_value rm rM rn rN,
+  rw ←eq_rM at nz_rM, rw ←eq_rN at nz_rN,
+  revert rm rM rn rN eq_rm eq_rM eq_rn eq_rN nz_rM nz_rN eq_x eq_y,
+  intros rm rM rn rN eq_rm eq_rM eq_rn eq_rN nz_rM nz_rN eq_x eq_y,
+  rw [eq_x, eq_y, div_pow, div_pow] at eqn,
+  have flat_eqn : rn^2 * rM^3 = (rm^3 + rM^3) * rN^2,
+  { calc rn^2 * rM^3 = rn^2 / rN^2 * rN^2 * rM^3 : 
+      by rw div_mul_cancel _ (pow_ne_zero 2 nz_rN)
+    ... = (rm^3 / rM^3 + 1) * rM^3 * rN^2 :
+      by rw eqn; ring_nf
+    ... = (rm^3 + rM^3) * rN^2 :
+      by rw [add_mul, div_mul_cancel _ (pow_ne_zero 3 nz_rM), one_mul] },
+    
+  rw [eq_rn, eq_rN, eq_rm, eq_rM] at flat_eqn,
+  simp_rw [←ring_hom.map_pow, ←ring_hom.map_mul, 
+    ←ring_hom.map_add, ←ring_hom.map_mul] at flat_eqn,
+  rw (ratfunc.algebra_map_injective k).eq_iff at flat_eqn,
+
+  -- TODO: x is mk m M and y is mk n N
+  -- TODO: is_const x if and only if m M are associated
+  -- associated_of_dvd_dvd -> N^2 and M^3 dividing each other gives associated
+  -- power -> all exponentials divided by 2 and 3
+
+  -- have coe_rm : rm = ↑m := eq_rm,
+  -- have coe_rM : rM = ↑M := eq_rM,
+  -- have coe_rn : rn = ↑n := eq_rn,
+  -- have coe_rN : rN = ↑N := eq_rN,
+  -- rw [coe_rm, coe_rM, coe_rn, coe_rN] at flat_eqn,
+  -- simp_rw [←algebra_map.coe_pow,
+  --   ←algebra_map.coe_add,
+  --   ←algebra_map.coe_mul] at flat_eqn,
+  
   sorry,
 end
