36e5a953929264d83ad9ef9616684fcd3d2304b9
Jineon Baek <jineon@umich.edu>
2023-04-05 20:46:07 -0400

    sorryfree

--- src/corollaries/no_parametrization.lean
+++ src/corollaries/no_parametrization.lean
@@ -84,6 +84,36 @@ begin
     ←associates.count_pow hB hp n, h],
 end
 
+example (p q : ℕ) (f: ℕ -> ℕ) (h : p = q) : f p = f q := congr_arg f h
+
+lemma is_const_of_associated {f : ratfunc k} (h : associated f.num f.denom) : 
+  is_const f := 
+begin
+  rcases h.symm with ⟨u, heq⟩,
+  rcases (polynomial.is_unit_iff.mp u.is_unit) with ⟨c, uc, eq_c⟩,
+  rw ←eq_c at heq,
+  have tt := congr_arg (algebra_map (polynomial k) (ratfunc k)) heq.symm,
+  rw [map_mul, mul_comm] at tt,
+  have uu := div_eq_of_eq_mul (algebra_map_ne_zero f.denom_ne_zero) tt,
+  rw [f.num_div_denom, ratfunc.algebra_map_C] at uu,
+  use c, exact uu,
+end
+
+lemma is_const_of_root_unity {n : ℕ} (hn : 0 < n) {f : ratfunc k}
+  (h : f^n = 1) : is_const f :=
+begin
+  rw [←f.num_div_denom, div_pow, ←map_pow, ←map_pow] at h,
+  have nz : algebra_map k[X] (ratfunc k) (f.denom ^ n) ≠ 0,
+  apply algebra_map_ne_zero, apply pow_ne_zero, exact f.denom_ne_zero,
+  rw [←mul_left_inj' nz, div_mul_cancel 
+    (algebra_map k[X] (ratfunc k) (f.num ^ n)) nz, one_mul] at h,
+  have hh := congr_arg associates.mk (algebra_map_injective k h),
+  rw [associates.mk_pow, associates.mk_pow,
+    associates_pow_eq_pow_iff hn,
+    associates.mk_eq_mk_iff_associated] at hh, 
+  exact is_const_of_associated hh,
+end
+
 theorem associated_pow_pow_coprime_iff {a b : k[X]} {m n : ℕ}
   (ha : a ≠ 0) (hb : b ≠ 0) (hm : 0 < m) (hn : 0 < n)
   (h : associated (a^m) (b^n)) (hcp : m.coprime n)
@@ -120,48 +150,39 @@ begin
   exact h.symm,
 end
 
-theorem no_parametrization_y2_x3_1 (chk : ¬(ring_char k ∣ 6))
-  {x y : ratfunc k} (eqn : y^2 = x^3 + 1) : is_const x ∧ is_const y :=
+lemma calcstep {n N m M : k[X]}
+  (nz_M : M ≠ 0) (nz_N : N ≠ 0)
+  (eqn : (algebra_map _ (ratfunc k)) n ^ 2 / (algebra_map _ (ratfunc k)) N ^ 2 =
+    (algebra_map _ (ratfunc k)) m ^ 3 / (algebra_map _ (ratfunc k)) M ^ 3 + 1) : 
+  n ^ 2 * M ^ 3 = (m ^ 3 + M ^ 3) * N ^ 2 :=
 begin
-  have eq_x := x.num_div_denom.symm,
-  have eq_y := y.num_div_denom.symm,
-  have nz_M := x.denom_ne_zero,
-  have nz_N := y.denom_ne_zero,
-  have cp_mM := is_coprime.num_denom x,
-  have cp_nN := is_coprime.num_denom y,
+  have nz_rM := ratfunc.algebra_map_ne_zero nz_M,
+  have nz_rN := ratfunc.algebra_map_ne_zero nz_N,
+  rw ←(ratfunc.algebra_map_injective k).eq_iff,
+  simp_rw [ring_hom.map_mul, ring_hom.map_add, ring_hom.map_pow],
+  set rm := algebra_map k[X] (ratfunc k) m with eq_rm,
+  set rM := algebra_map k[X] (ratfunc k) M with eq_rM,
+  set rn := algebra_map k[X] (ratfunc k) n with eq_rn,
+  set rN := algebra_map k[X] (ratfunc k) N with eq_rN,
+  rw [←eq_rm, ←eq_rM, ←eq_rn, ←eq_rN],
+  calc rn^2 * rM^3 = rn^2 / rN^2 * rN^2 * rM^3 : 
+      by rw div_mul_cancel _ (pow_ne_zero 2 nz_rN)
+    ... = (rm^3 / rM^3 + 1) * rM^3 * rN^2 :
+      by rw [eqn, mul_assoc, mul_assoc, mul_comm (rN^2) _]
+    ... = (rm^3 + rM^3) * rN^2 :
+      by rw [add_mul, div_mul_cancel _ (pow_ne_zero 3 nz_rM), one_mul],
+end
 
-  set m := x.num with eq_m,
-  set M := x.denom with eq_M,
-  set n := y.num with eq_n,
-  set N := y.denom with eq_N,
-  clear_value m M n N,
-  rw [eq_x, eq_y] at eqn,
-  simp only [div_pow] at eqn,
-
-  revert m M n N eq_m eq_M eq_n eq_N nz_M nz_N eq_x eq_y eqn cp_mM cp_nN,
-  intros m M n N eq_m eq_M eq_n eq_N nz_M nz_N eq_x eq_y eqn cp_mM cp_nN,
-
-  have nz_m : m ≠ 0, sorry,
-  have nz_n : n ≠ 0, sorry,
-
-  have flat_eqn : n ^ 2 * M ^ 3 = (m ^ 3 + M ^ 3) * N ^ 2,
-  { have nz_rM := ratfunc.algebra_map_ne_zero nz_M,
-    have nz_rN := ratfunc.algebra_map_ne_zero nz_N,
-    rw ←(ratfunc.algebra_map_injective k).eq_iff,
-    simp_rw [ring_hom.map_mul, ring_hom.map_add, ring_hom.map_pow],
-    set rm := algebra_map k[X] (ratfunc k) m with eq_rm,
-    set rM := algebra_map k[X] (ratfunc k) M with eq_rM,
-    set rn := algebra_map k[X] (ratfunc k) n with eq_rn,
-    set rN := algebra_map k[X] (ratfunc k) N with eq_rN,
-    rw [←eq_rm, ←eq_rM, ←eq_rn, ←eq_rN],
-    calc rn^2 * rM^3 = rn^2 / rN^2 * rN^2 * rM^3 : 
-        by rw div_mul_cancel _ (pow_ne_zero 2 nz_rN)
-      ... = (rm^3 / rM^3 + 1) * rM^3 * rN^2 :
-        by rw eqn; ring_nf
-      ... = (rm^3 + rM^3) * rN^2 :
-        by rw [add_mul, div_mul_cancel _ (pow_ne_zero 3 nz_rM), one_mul], },
-  clear eqn,
-
+lemma calcstep2 
+  {m M n N : k[X]} (nz_M : M ≠ 0) (nz_N : N ≠ 0)
+  (cp_mM : is_coprime m M)
+  (cp_nN : is_coprime n N)
+  (nz_m : m ≠ 0)
+  (nz_n : n ≠ 0)
+  (flat_eqn : n ^ 2 * M ^ 3 = (m ^ 3 + M ^ 3) * N ^ 2) : 
+  ∃ (w : k[X]) (u v : k[X]ˣ), w ≠ 0 ∧ ↑u * w ^ 2 = M ∧ ↑v * w ^ 3 = N ∧ is_coprime m w ∧ 
+    ↑(u ^ 3) * n ^ 2 = ↑(v ^ 2) * m ^ 3 + ↑(v ^ 2 * u ^ 3) * w ^ 6 :=
+begin
   have assoc_M3_N2 : associated (M^3) (N^2),
   { apply associated_of_dvd_dvd,
     { have cp : is_coprime (M^3) (m^3+1*M^3) := 
@@ -174,11 +195,14 @@ begin
       rw flat_eqn, exact dvd_mul_left _ _, }, },
   
   rcases (associated_pow_pow_coprime_iff nz_M nz_N 
-    dec_trivial _ assoc_M3_N2 (by norm_num))
+  dec_trivial _ assoc_M3_N2 (by norm_num))
     with ⟨w, nz_w, assoc_M_w2, assoc_N_w3⟩,
   swap, dec_trivial,
   rcases assoc_M_w2.symm with ⟨u, eq_Mw⟩, rw mul_comm at eq_Mw,
   rcases assoc_N_w3.symm with ⟨v, eq_Nw⟩, rw mul_comm at eq_Nw,
+
+  refine ⟨w, u, v, nz_w, eq_Mw, eq_Nw, _⟩,
+
   simp_rw [←eq_Mw, ←eq_Nw, mul_pow, ←pow_mul, ←mul_assoc] at flat_eqn,
   ring_nf at flat_eqn,
   rw mul_left_inj' (pow_ne_zero 6 nz_w) at flat_eqn,
@@ -189,12 +213,72 @@ begin
   have cp_mw : is_coprime m w,
   { rw [←eq_Mw, is_coprime_mul_unit_left_right u.is_unit,
       is_coprime.pow_right_iff] at cp_mM, 
-    exact cp_mM, dec_trivial },
+    exact cp_mM, dec_trivial }, refine ⟨cp_mw, flat_eqn⟩, 
+end
+
+theorem no_parametrization_y2_x3_1 (chk : ¬(ring_char k ∣ 6))
+  {x y : ratfunc k} (eqn : y^2 = x^3 + 1) : is_const x ∧ is_const y :=
+begin
+  cases eq_or_ne x 0 with eq_x nz_x,
+  { subst x, 
+    rw [zero_pow', zero_add] at eqn, swap, dec_trivial,
+    refine ⟨⟨0, (map_zero _).symm⟩, is_const_of_root_unity _ eqn⟩,
+    dec_trivial },
+  cases eq_or_ne y 0 with eq_y nz_y,
+  { subst y, rw [eq_comm, zero_pow', ←eq_neg_iff_add_eq_zero] at eqn,
+    swap, dec_trivial,
+    have tt : (x^3)^2 = 1, rw sq_eq_one_iff, right, exact eqn,
+    ring_nf at tt,
+    refine ⟨is_const_of_root_unity _ tt, ⟨0, (map_zero _).symm⟩⟩,
+    dec_trivial },
+
+  have eq_x := x.num_div_denom.symm,
+  have eq_y := y.num_div_denom.symm,
+  have nz_m := num_ne_zero nz_x,
+  have nz_n := num_ne_zero nz_y,
+  have nz_M := x.denom_ne_zero,
+  have nz_N := y.denom_ne_zero,
+  have cp_mM := is_coprime.num_denom x,
+  have cp_nN := is_coprime.num_denom y,
+
+  set m := x.num,
+  set M := x.denom,
+  set n := y.num,
+  set N := y.denom,
+  clear_value m M n N,
+  rw [eq_x, eq_y] at eqn,
+  simp only [div_pow] at eqn,
+  subst eq_x, subst eq_y,
+ 
+  have flat_eqn : n ^ 2 * M ^ 3 = (m ^ 3 + M ^ 3) * N ^ 2,
+  { apply calcstep; assumption },
+  clear eqn,
+
+  have eqn2 : 
+  ∃ (w : k[X]) (u v : k[X]ˣ), w ≠ 0 ∧ ↑u * w ^ 2 = M ∧ ↑v * w ^ 3 = N ∧ is_coprime m w ∧ 
+    ↑(u ^ 3) * n ^ 2 = ↑(v ^ 2) * m ^ 3 + ↑(v ^ 2 * u ^ 3) * w ^ 6,
+  { apply calcstep2; assumption },
+  clear flat_eqn, 
+  rcases eqn2 with ⟨w, u, v, nz_w, eq_M, eq_N, cp_mw, eqn2⟩, 
   have chk2 : ¬ring_char k ∣ 2,
   { intro h, apply chk, apply h.trans, use 3, norm_num, },
   have chk3 : ¬ring_char k ∣ 3,
   { intro h, apply chk, apply h.trans, use 2, norm_num, },
+
+  rw [eq_comm, ←sub_eq_zero, sub_eq_add_neg, ←neg_mul, ←units.coe_neg] at eqn2,
   have deriv_eq_zero := polynomial.flt_catalan 
-    _ _ _ _ chk3 chk chk2 nz_m nz_w nz_n cp_mw flat_eqn.symm; try {norm_num},
+    _ _ _ _ chk3 chk chk2 nz_m nz_w nz_n cp_mw eqn2,
   rcases deriv_eq_zero with ⟨eq_dm, eq_dw, eq_dn⟩,
+  split,
+  { rw (polynomial.eq_C_of_nat_degree_eq_zero eq_dm),
+    rw ←eq_M, rw (polynomial.eq_C_of_nat_degree_eq_zero eq_dw),
+    rcases polynomial.is_unit_iff.mp u.is_unit with ⟨cu, -, eq_cu⟩,
+    rw ←eq_cu, rw [←map_pow, ←map_mul, ratfunc.algebra_map_C,
+      ratfunc.algebra_map_C, ←map_div₀, is_const], existsi _, refl, },
+  { rw (polynomial.eq_C_of_nat_degree_eq_zero eq_dn),
+    rw ←eq_N, rw (polynomial.eq_C_of_nat_degree_eq_zero eq_dw),
+    rcases polynomial.is_unit_iff.mp v.is_unit with ⟨cv, -, eq_cv⟩,
+    rw ←eq_cv, rw [←map_pow, ←map_mul, ratfunc.algebra_map_C,
+      ratfunc.algebra_map_C, ←map_div₀, is_const], existsi _, refl, },
+  iterate 4 { norm_num }, 
 end
