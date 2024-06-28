f8c34d049baf6b2267ba07d6443006eb239eefa7
Seewoo Lee <seewoo5@gmail.com>
2024-06-28 11:38:46 -0700

    remove import

--- LeanPolyABC/All.lean
+++ LeanPolyABC/All.lean
@@ -2,7 +2,6 @@ import LeanPolyABC.MasonStothers
 import LeanPolyABC.Lib.DivRadical
 import LeanPolyABC.Lib.Max3
 import LeanPolyABC.Lib.Wronskian
-import LeanPolyABC.Lib.RationalFunc
 import LeanPolyABC.Lib.Radical
 import LeanPolyABC.Corollaries.FltCatalan
 import LeanPolyABC.Corollaries.Davenport
