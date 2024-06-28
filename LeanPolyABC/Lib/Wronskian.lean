9f5aca595022b2632c116beb81a9cdd2665230a8
Seewoo Lee <seewoo5@gmail.com>
2024-06-28 15:12:21 -0700

    k->R

--- LeanPolyABC/Lib/Wronskian.lean
+++ LeanPolyABC/Lib/Wronskian.lean
@@ -6,35 +6,35 @@ open scoped Polynomial Classical
 
 open Polynomial
 
-variable {k : Type _} [CommRing k]
+variable {R : Type _} [CommRing R]
 
 /-- Wronskian: W(a, b) = ab' - a'b. -/
-def wronskian (a b : k[X]) : k[X] :=
+def wronskian (a b : R[X]) : R[X] :=
   a * (derivative b) - (derivative a) * b
 
 @[simp]
-theorem wronskian_zero_left (a : k[X]) : wronskian 0 a = 0 := by
+theorem wronskian_zero_left (a : R[X]) : wronskian 0 a = 0 := by
   simp_rw [wronskian]; simp only [MulZeroClass.zero_mul, derivative_zero, sub_self]
 
 @[simp]
-theorem wronskian_zero_right (a : k[X]) : wronskian a 0 = 0 := by
+theorem wronskian_zero_right (a : R[X]) : wronskian a 0 = 0 := by
   simp_rw [wronskian]; simp only [derivative_zero, MulZeroClass.mul_zero, sub_self]
 
-theorem wronskian_neg_left (a b : k[X]) : wronskian (-a) b = -wronskian a b := by
+theorem wronskian_neg_left (a b : R[X]) : wronskian (-a) b = -wronskian a b := by
   simp_rw [wronskian, derivative_neg]; ring
 
-theorem wronskian_neg_right (a b : k[X]) : wronskian a (-b) = -wronskian a b := by
+theorem wronskian_neg_right (a b : R[X]) : wronskian a (-b) = -wronskian a b := by
   simp_rw [wronskian, derivative_neg]; ring
 
-theorem wronskian_add_right (a b c : k[X]) : wronskian a (b + c) = wronskian a b + wronskian a c :=
+theorem wronskian_add_right (a b c : R[X]) : wronskian a (b + c) = wronskian a b + wronskian a c :=
   by simp_rw [wronskian, derivative_add]; ring
 
-theorem wronskian_self (a : k[X]) : wronskian a a = 0 := by rw [wronskian, mul_comm, sub_self]
+theorem wronskian_self (a : R[X]) : wronskian a a = 0 := by rw [wronskian, mul_comm, sub_self]
 
-theorem wronskian_anticomm (a b : k[X]) : wronskian a b = -wronskian b a := by
+theorem wronskian_anticomm (a b : R[X]) : wronskian a b = -wronskian b a := by
   rw [wronskian, wronskian]; ring
 
-theorem wronskian_eq_of_sum_zero {a b c : k[X]} (h : a + b + c = 0) :
+theorem wronskian_eq_of_sum_zero {a b c : R[X]} (h : a + b + c = 0) :
     wronskian a b = wronskian b c :=
   by
   rw [← neg_eq_iff_add_eq_zero] at h
@@ -45,10 +45,10 @@ theorem wronskian_eq_of_sum_zero {a b c : k[X]} (h : a + b + c = 0) :
   rw [add_zero]
   rw [← wronskian_anticomm]
 
-private theorem degree_ne_bot {a : k[X]} (ha : a ≠ 0) : a.degree ≠ ⊥ := by
+private theorem degree_ne_bot {a : R[X]} (ha : a ≠ 0) : a.degree ≠ ⊥ := by
   intro h; rw [Polynomial.degree_eq_bot] at h; exact ha h
 
-theorem wronskian.degree_lt_add {a b : k[X]} (ha : a ≠ 0) (hb : b ≠ 0) :
+theorem wronskian.degree_lt_add {a b : R[X]} (ha : a ≠ 0) (hb : b ≠ 0) :
     (wronskian a b).degree < a.degree + b.degree :=
   by
   calc
@@ -76,11 +76,11 @@ theorem wronskian.degree_lt_add {a b : k[X]} (ha : a ≠ 0) (hb : b ≠ 0) :
 -- Counterexample: b = a = 1 →
 -- (wronskian a b).natDegree = a.natDegree = b.natDegree = 0
 /-
-lemma wronskian.natDegree_lt_add {a b : k[X]}
+lemma wronskian.natDegree_lt_add {a b : R[X]}
   (ha : a ≠ 0) (hb : b ≠ 0) :
   (wronskian a b).natDegree < a.natDegree + b.natDegree := sorry
 -/
-theorem wronskian.natDegree_lt_add {a b : k[X]} (hw : wronskian a b ≠ 0) :
+theorem wronskian.natDegree_lt_add {a b : R[X]} (hw : wronskian a b ≠ 0) :
     (wronskian a b).natDegree < a.natDegree + b.natDegree :=
   by
   have ha : a ≠ 0 := by intro h; subst h; rw [wronskian_zero_left] at hw; exact hw rfl
