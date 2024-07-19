5a655776e207034ffbb07f8e571aa5e20334fefc
Seewoo Lee <49933279+seewoo5@users.noreply.github.com>
2024-07-19 00:21:05 -0700

    Reordering & renaming (#54)
    * renaming
    
    * reordering

--- LeanPolyABC/Lib/Radical.lean
+++ LeanPolyABC/Lib/Radical.lean
@@ -19,28 +19,58 @@ def primeFactors (a : α) : Finset α :=
 def radical (a : α) : α :=
   (primeFactors a).prod id
 
-theorem radical_zero : radical (0 : α) = 1 := by
+theorem radical_zero_eq_one : radical (0 : α) = 1 := by
   rw [radical, primeFactors, normalizedFactors_zero, Multiset.toFinset_zero, Finset.prod_empty]
 
-theorem radical_one : radical (1 : α) = 1 := by
+theorem radical_one_eq_one : radical (1 : α) = 1 := by
   rw [radical, primeFactors, normalizedFactors_one, Multiset.toFinset_zero, Finset.prod_empty]
 
-theorem radical_associated {a b : α} (h : Associated a b) : radical a = radical b :=
+theorem radical_associated_eq {a b : α} (h : Associated a b) : radical a = radical b :=
   by
   rcases iff_iff_and_or_not_and_not.mp h.eq_zero_iff with (⟨rfl, rfl⟩ | ⟨ha, hb⟩)
   · rfl
   · simp_rw [radical, primeFactors]
     rw [(associated_iff_normalizedFactors_eq_normalizedFactors ha hb).mp h]
 
-theorem radical_isUnit {a : α} (h : IsUnit a) : radical a = 1 :=
-  (radical_associated (associated_one_iff_isUnit.mpr h)).trans radical_one
-
-theorem radical_unit {u : αˣ} : radical (↑u : α) = 1 :=
-  radical_isUnit u.isUnit
+theorem radical_unit_eq_one {a : α} (h : IsUnit a) : radical a = 1 :=
+  (radical_associated_eq (associated_one_iff_isUnit.mpr h)).trans radical_one_eq_one
 
 theorem radical_unit_hMul {u : αˣ} {a : α} : radical ((↑u : α) * a) = radical a :=
-  radical_associated (associated_unit_mul_left _ _ u.isUnit)
+  radical_associated_eq (associated_unit_mul_left _ _ u.isUnit)
 
+theorem primeFactors_pow (a : α) {n : ℕ} (hn : 0 < n) : primeFactors (a ^ n) = primeFactors a :=
+  by
+  simp_rw [primeFactors]
+  simp only [normalizedFactors_pow]
+  rw [Multiset.toFinset_nsmul]
+  exact ne_of_gt hn
+
+theorem radical_pow (a : α) {n : Nat} (hn : 0 < n) : radical (a ^ n) = radical a := by
+  simp_rw [radical, primeFactors_pow a hn]
+
+theorem radical_dvd_self (a : α) : radical a ∣ a :=
+  by
+  by_cases ha : a = 0
+  · rw [ha]
+    apply dvd_zero
+  · rw [radical, ← Finset.prod_val, ← (normalizedFactors_prod ha).dvd_iff_dvd_right]
+    apply Multiset.prod_dvd_prod_of_le
+    rw [primeFactors, Multiset.toFinset_val]
+    apply Multiset.dedup_le
+
+theorem radical_prime {a : α} (ha : Prime a) : radical a = normalize a :=
+  by
+  rw [radical, primeFactors]
+  rw [normalizedFactors_irreducible ha.irreducible]
+  simp only [Multiset.toFinset_singleton, id, Finset.prod_singleton]
+
+theorem radical_prime_pow {a : α} (ha : Prime a) {n : ℕ} (hn : 1 ≤ n) :
+    radical (a ^ n) = normalize a := by
+  rw [radical_pow a hn]
+  exact radical_prime ha
+
+
+-- Theorems for commutative rings
 variable {R : Type _} [CommRing R] [IsDomain R] [NormalizationMonoid R] [UniqueFactorizationMonoid R]
 
 /-- coprime polynomials have disjoint prime factors (as multisets). -/
@@ -69,43 +99,24 @@ private theorem IsCoprime.hMul_primeFactors_disjUnion {a b : R} (ha : a ≠ 0) (
 -- possible TODO: the proof is unnecessarily long
 @[simp]
 theorem radical_neg_one : radical (-1 : R) = 1 :=
-  radical_isUnit isUnit_one.neg
+  radical_unit_eq_one isUnit_one.neg
 
 theorem radical_hMul {a b : R} (hc : IsCoprime a b) : radical (a * b) = (radical a) * (radical b) :=
   by
   by_cases ha : a = 0
   · subst ha; rw [isCoprime_zero_left] at hc
-    simp only [MulZeroClass.zero_mul, radical_zero, one_mul, radical_isUnit hc]
+    simp only [MulZeroClass.zero_mul, radical_zero_eq_one, one_mul, radical_unit_eq_one hc]
   by_cases hb : b = 0
   · subst hb; rw [isCoprime_zero_right] at hc
-    simp only [MulZeroClass.mul_zero, radical_zero, mul_one, radical_isUnit hc]
+    simp only [MulZeroClass.mul_zero, radical_zero_eq_one, mul_one, radical_unit_eq_one hc]
   simp_rw [radical]
   rw [hc.hMul_primeFactors_disjUnion ha hb]
   rw [Finset.prod_disjUnion hc.disjoint_primeFactors]
 
 theorem radical_neg {a : R} : radical (-a) = radical a :=
-  neg_one_mul a ▸ (radical_associated <| associated_unit_mul_left a (-1) isUnit_one.neg)
+  neg_one_mul a ▸ (radical_associated_eq <| associated_unit_mul_left a (-1) isUnit_one.neg)
 
-theorem primeFactors_pow (a : α) {n : ℕ} (hn : 0 < n) : primeFactors (a ^ n) = primeFactors a :=
-  by
-  simp_rw [primeFactors]
-  simp only [normalizedFactors_pow]
-  rw [Multiset.toFinset_nsmul]
-  exact ne_of_gt hn
-
-theorem radical_pow (a : α) {n : Nat} (hn : 0 < n) : radical (a ^ n) = radical a := by
-  simp_rw [radical, primeFactors_pow a hn]
-
-theorem radical_dvd_self (a : α) : radical a ∣ a :=
-  by
-  by_cases ha : a = 0
-  · rw [ha]
-    apply dvd_zero
-  · rw [radical, ← Finset.prod_val, ← (normalizedFactors_prod ha).dvd_iff_dvd_right]
-    apply Multiset.prod_dvd_prod_of_le
-    rw [primeFactors, Multiset.toFinset_val]
-    apply Multiset.dedup_le
-
+-- This actually holds for nontrivial monoids - do not need ring assumption.
 theorem radical_ne_zero (a : R) : radical a ≠ 0 :=
   by
   rw [radical, ← Finset.prod_val]
@@ -113,17 +124,6 @@ theorem radical_ne_zero (a : R) : radical a ≠ 0 :=
   rw [primeFactors]
   simp only [Multiset.toFinset_val, Multiset.mem_dedup]
   exact zero_not_mem_normalizedFactors _
-
-theorem radical_prime {a : R} (ha : Prime a) : radical a = normalize a :=
-  by
-  rw [radical, primeFactors]
-  rw [normalizedFactors_irreducible ha.irreducible]
-  simp only [Multiset.toFinset_singleton, id, Finset.prod_singleton]
-
-theorem radical_prime_pow {a : R} (ha : Prime a) {n : ℕ} (hn : 1 ≤ n) :
-    radical (a ^ n) = normalize a := by
-  rw [radical_pow a hn]
-  exact radical_prime ha
 
 namespace Polynomial
 
@@ -133,7 +133,7 @@ theorem radical_degree_le {a : k[X]} (ha : a ≠ 0) : (radical a).degree ≤ a.d
 theorem radical_natDegree_le {a : k[X]} : (radical a).natDegree ≤ a.natDegree :=
   by
   by_cases ha : a = 0
-  · rw [ha, radical_zero, natDegree_one, natDegree_zero]
+  · rw [ha, radical_zero_eq_one, natDegree_one, natDegree_zero]
   · exact natDegree_le_of_dvd (radical_dvd_self a) ha
 
 end Polynomial
