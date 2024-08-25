af0877c8824333cd5385b64a63f45036a6c9d6bd
Jineon Baek <34874130+jcpaik@users.noreply.github.com>
2024-08-25 09:18:22 +0900

    True Davenport's theorem with no coprimality (#57)

--- LeanPolyABC/Corollaries/Davenport.lean
+++ LeanPolyABC/Corollaries/Davenport.lean
@@ -12,6 +12,28 @@ open UniqueFactorizationMonoid
 
 variable {k : Type _} [Field k]
 
+/-- Davenport's theorem
+
+For any nonzero polynomial a, b ∈ k[t] with k of characteristic zero, deg(a) + 2 ≤ 2 * deg(a^3 - b^2).
+
+Proof) Apply ABC for (-a^3, b^2, a^3 - b^2).
+-/
+lemma ne_zero_of_natDegree_gt_0 {a : k[X]} (ha : a.natDegree > 0) : a ≠ 0 := λ h ↦ by
+  simp only [h, natDegree_zero, gt_iff_lt, lt_self_iff_false] at ha
+
+theorem Polynomial.davenport [CharZero k] {a b : k[X]}
+    (ha : a.natDegree > 0) (hb : b.natDegree > 0) (hnz : a ^ 3 - b ^ 2 ≠ 0) :
+    a.natDegree + 2 ≤ 2 * (a ^ 3 - b ^ 2).natDegree := by
+  have ha3 : -a^3 ≠ 0 := neg_ne_zero.mpr <| pow_ne_zero 3 <| ne_zero_of_natDegree_gt_0 ha
+  have hb2 : b^2 ≠ 0 := pow_ne_zero 2 <| ne_zero_of_natDegree_gt_0 hb
+  cases' abc'_char0 ha3 hb2 hnz (by ring_nf) with heq hineq
+  · simp only [natDegree_neg, natDegree_pow] at heq
+    omega
+  · rw [Nat.succ_le_iff] at hineq
+    simp only [Nat.max₃, natDegree_neg, natDegree_pow,
+      radical_neg, Nat.ofNat_pos, radical_pow, max_lt_iff] at hineq
+    nlinarith only [hineq.1, hineq.2, radical_natDegree_le a, radical_natDegree_le b]
+
 -- Auxiliary lemma to remove nonzero hypothesis using coprimality and a' ≠ 0.
 theorem isCoprime_nonzero_c {a b : k[X]} (h : IsCoprime a b) (ha : derivative a ≠ 0) :
     a ^ 3 - b ^ 2 ≠ 0 := by
@@ -23,14 +45,12 @@ theorem isCoprime_nonzero_c {a b : k[X]} (h : IsCoprime a b) (ha : derivative a 
   rw [← eq_a] at ha; exact ha derivative_C
   norm_num
 
--- 3 ≠ 0
-/- Davenport's theorem
-For any coprime polynomial a, b ∈ k[t] with nonzero derivatives,
-deg(a) + 2 ≤ 2 * deg(a^3 - b^2).
+/-- Davenport's theorem for general field k of any characteristic
 
+For any coprime polynomial a, b ∈ k[t] with nonzero derivatives, deg(a) + 2 ≤ 2 * deg(a^3 - b^2).
 Proof) Apply ABC for (-a^3, b^2, a^3 - b^2).
 -/
-theorem Polynomial.davenport {a b : k[X]} (hab : IsCoprime a b) (haderiv : derivative a ≠ 0)
+theorem Polynomial.davenport' {a b : k[X]} (hab : IsCoprime a b) (haderiv : derivative a ≠ 0)
     (hbderiv : derivative b ≠ 0) : a.natDegree + 2 ≤ 2 * (a ^ 3 - b ^ 2).natDegree :=
   by
   have hnz : a ^ 3 - b ^ 2 ≠ 0 := isCoprime_nonzero_c hab haderiv
@@ -43,10 +63,16 @@ theorem Polynomial.davenport {a b : k[X]} (hab : IsCoprime a b) (haderiv : deriv
     rwa [sub_eq_add_neg, neg_eq_neg_one_mul, IsCoprime.add_mul_right_right_iff,
       IsCoprime.pow_iff two_pos three_pos, isCoprime_comm]
   cases'
-    abc (neg_ne_zero.mpr (pow_ne_zero 3 ha)) (pow_ne_zero 2 hb) hnz hab.pow.neg_left h2
-      h1.neg_left.symm
+    abc (neg_ne_zero.mpr (pow_ne_zero 3 ha)) (pow_ne_zero 2 hb) hnz hab.pow.neg_left
       (by rw [sub_eq_add_neg, add_add_add_comm, neg_add_self, add_neg_self, add_zero]) with
     h h
+  · -- When we have vanishing derivatives. This condition implies 3 = 0 = 2, which is impossible for any field k.
+    rw [derivative_neg, neg_eq_zero, derivative_pow, derivative_pow, Nat.add_one_sub_one,
+      Nat.add_one_sub_one, mul_eq_zero, mul_eq_zero, mul_eq_zero, mul_eq_zero, or_iff_left haderiv,
+      or_iff_left hbderiv, or_iff_left (pow_ne_zero 2 ha), or_iff_left (pow_ne_zero 1 hb)] at h
+    replace h := h.1.trans h.2.1.symm
+    rw [← sub_eq_zero, ← C_sub, ← Nat.cast_sub (Nat.le_succ 2), Nat.cast_one, C_1] at h
+    exact (one_ne_zero h).elim
   · -- When we have inequality from ABC.
     rw [natDegree_neg, Nat.max₃, max_eq_left (natDegree_sub_le _ _), neg_mul, neg_mul, radical_neg,
       radical_hMul (h1.mul_left h2), radical_hMul hab.pow, radical_pow a three_pos,
@@ -56,14 +82,7 @@ theorem Polynomial.davenport {a b : k[X]} (hab : IsCoprime a b) (haderiv : deriv
       max_add_add_right] at h
     replace h :=
       le_trans h
-        (add_le_add (add_le_add radical_natDegree_le radical_natDegree_le) radical_natDegree_le)
+        (add_le_add (add_le_add (radical_natDegree_le _) (radical_natDegree_le _)) <| radical_natDegree_le _)
     rw [max_le_iff] at h
     -- Add two inequalities and simplifying it gives the desired inequality.
     nlinarith only [add_le_add h.1 h.2]
-  · -- When we have vanishing derivatives. This condition implies 3 = 0 = 2, which is impossible for any field k.
-    rw [derivative_neg, neg_eq_zero, derivative_pow, derivative_pow, Nat.add_one_sub_one,
-      Nat.add_one_sub_one, mul_eq_zero, mul_eq_zero, mul_eq_zero, mul_eq_zero, or_iff_left haderiv,
-      or_iff_left hbderiv, or_iff_left (pow_ne_zero 2 ha), or_iff_left (pow_ne_zero 1 hb)] at h
-    replace h := h.1.trans h.2.1.symm
-    rw [← sub_eq_zero, ← C_sub, ← Nat.cast_sub (Nat.le_succ 2), Nat.cast_one, C_1] at h
-    exact (one_ne_zero h).elim
